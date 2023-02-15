c1d7e262955df40dec29f2ef8b105fa98afc9660
Gichogomary <marygichogo53@gmail.com>
2023-02-15 10:48:19 +0300

    added calculator functions

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#this is a calculator project
+num1 = eval(input("enter first number:"))
+num2 = eval(input("enter second number:"))
+operator = input("enter operator:")
+def add(num1,num2):
+   result = num1+num2
+   return result
